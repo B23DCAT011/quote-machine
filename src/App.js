74ce973f6e298a915a074304892e2bb395a3c224
B23DCAT011 <ducanhluust@gmail.com>
2024-05-11 15:35:57 +0700

    up 11/05/2024

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import './App.scss';
+import mdColors from './colors';
+import quotess from './quoteBank';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faTwitter} from '@fortawesome/free-brands-svg-icons'
+
+
+
+
+  
 
 function App() {
+
+  const [quote,setQuote] = useState("The only way to do great work is to love what you do.")
+  const [author,setAuthor] = useState('Steve Jobs')
+  const [randomNumber,setrandomNumber] = useState(0)
+  const [accentColor,setaccentColor] = useState(`#282c34`)
+  
+
+  const getRandomQuote = () =>{
+    let randomInterger = Math.floor((quotess.length)*Math.random())
+    setrandomNumber(randomInterger)
+    setaccentColor(mdColors[randomInterger])
+    setQuote(quotess[randomInterger].quote)
+    setAuthor(quotess[randomInterger].author)
+  }
+
+
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <header className="App-header" style={{backgroundColor:accentColor}}>
+      <div id='quote-box'>
+        <p id='text' style={{color:accentColor}}>
+          {quote}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p id='author' style={{color:accentColor}}>- {author}</p>
+       <div id='button'>
+        <a style={{backgroundColor:accentColor,color:'white'}} id='tweet-quote' href={encodeURI(`"twitter.com/intent/tweet?t${quote} -${author}`)} ><FontAwesomeIcon icon={faTwitter} /></a>
+        <button style={{backgroundColor:accentColor,color:'white'}} id='new-quote' onClick={() => getRandomQuote()}>New quote</button>
+       </div>
+        
+      </div>
+    </header>
+   </div>
   );
 }
 
